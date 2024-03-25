a3e136136da3aa48c4361608063b252cc0a105d4
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-03-25 04:51:43 +0200

    adding box in sidebar

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,7 +1,9 @@
 import { cn } from '@/lib/utils';
 import React, { FC } from 'react';
 import { SidebarContext } from './Navbar';
-import { CloseIcon, DressIcon } from './Icons';
+import { CloseIcon, DressIcon, MdHeadphones } from './Icons';
+import { Button } from '../ui/button';
+import Image from 'next/image';
 
 export const Sidebar = () => {
   const { isVisible, hide } = React.useContext(SidebarContext);
@@ -16,7 +18,9 @@ export const Sidebar = () => {
       )}
     >
       <section
-        className={cn('bg-background p-12 overflow-y-auto md:w-96 w-full relative border')}
+        className={cn(
+          'bg-background flex flex-col p-12 overflow-y-auto md:w-96 w-full relative border'
+        )}
       >
         <button
           onClick={hide}
@@ -25,16 +29,38 @@ export const Sidebar = () => {
         >
           <CloseIcon className='size-6 md:size-5' />
         </button>
-        <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-6'>
           <h2 className='font-bold text-2xl'>Categories</h2>
-          <ul className='flex flex-col gap-2'>
-            {Array.from({ length: 20 }).map((item, i) => (
+          <ul className='flex flex-col gap-2 pb-12 border-b'>
+            {Array.from({ length: 5 }).map((item, i) => (
               <Item key={i} icon={<DressIcon />}>
                 Dresses and Suits
               </Item>
             ))}
           </ul>
         </div>
+
+        <div className='w-full gap-6 p-10 bg-muted rounded-md shrink-0 flex flex-col items-start my-10'>
+          <Image
+            src={'assets/babyshop.svg'}
+            width={10}
+            height={10}
+            alt='Image'
+            className='w-[100px] h-auto'
+          />
+          <h3 className='text-xl  font-bold'>Sweater</h3>
+          <Button className='w-full'>Show Details</Button>
+        </div>
+
+        <a
+          href='tel:+201234566'
+          className='text-sm flex items-center gap-2 group'
+        >
+          <MdHeadphones className='size-6' />
+          <span className='text-muted-foreground hidden md:inline-block group-hover:text-foreground'>
+            +201234566
+          </span>
+        </a>
       </section>
       {/* Overlay */}
       <section
